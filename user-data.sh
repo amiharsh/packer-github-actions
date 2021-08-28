2a7a51f756eeb6f7c18fc274347184df5e526d63
amiharsh <hvkataria12@gmail.com>
2021-08-28 16:13:45 +0530

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo -i
+echo "a1b2c3d4e5" | passwd ubuntu --stdin
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--no-deploy traefik" sh -s -
 sudo cat >/var/lib/rancher/k3s/server/manifests/ingress-nginx.yaml <<EOF
 apiVersion: v1
