02a9ac3fa544aecf7a1aca2a51808f64c9ba36ec
amiharsh <hvkataria12@gmail.com>
2021-08-28 17:38:54 +0530

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -9,37 +9,6 @@ service sshd restart
 echo "a1b2c3d4e5" | passwd ubuntu --stdin
 service sshd restart
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--no-deploy traefik" sh -s -
-cat >/var/lib/rancher/k3s/server/manifests/ingress-nginx.yaml <<EOF
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: ingress-nginx
----
-apiVersion: helm.cattle.io/v1
-kind: HelmChart
-metadata:
-  name: ingress-nginx
-  namespace: kube-system
-spec:
-  chart: ingress-nginx
-  repo: https://kubernetes.github.io/ingress-nginx
-  targetNamespace: ingress-nginx
-  version: v3.29.0
-  set:
-  valuesContent: |-
-    fullnameOverride: ingress-nginx
-    controller:
-      kind: DaemonSet
-      hostNetwork: true
-      hostPort:
-        enabled: true
-      service:
-        enabled: false
-      publishService:
-        enabled: false
-      config:
-        use-forwarded-headers: "true"
-EOF
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 helm install rfnchart /opt/rfnchart
 
