cc318c7b7bb965d3be0624b98af74b2026711f2a
amiharsh <hvkataria12@gmail.com>
2021-08-28 16:22:02 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,8 @@ sudo systemctl enable docker
 sudo usermod -aG docker ${USER}
 newgrp docker
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/lock_passwd: true/lock_passwd: false/g' /etc/cloud/cloud.cfg
 wget https://get.helm.sh/helm-v3.6.3-linux-amd64.tar.gz
 tar -zxvf helm-v3.6.3-linux-amd64.tar.gz
 mv linux-amd64/helm /usr/local/bin/helm
